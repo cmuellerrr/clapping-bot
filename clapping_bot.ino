ab1544e3ff60a42b99663050a4fc398f39e439b6
Chris Mueller <cmuellerrr@gmail.com>
2014-04-19 12:41:05 -0700

    fixed issue where first note was skipped, added status led for starting and shifting

--- clapping_bot.ino
+++ clapping_bot.ino
@@ -5,30 +5,49 @@
  * Currently a proof of concept with LEDs instead of solenoids.
  */
 
-
+const int beatLength = 200;
 int lastBeat = 0;
 int timePassed = 0;
-const int beatLength = 200;
-
-int p1[] = {1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0};
-int p2[] = {1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0};
-int curNote = 0;
-int curRep = 0;
-int curPhase = 0;
 
 const int numNotes = 12;
 const int numReps = 8;
+const int numPhases = 12;
+int curNote; 
+int curRep;
+int curPhase;
+boolean justShifted;
 
-int led1 = 3;
-int led2 = 4;
+int pattern1[] = {1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0};
+int pattern2[] = {1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0};
+
+const int player1 = 3;
+const int player2 = 4;
+const int statusLED = 13;
 
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
+  pinMode(player1, OUTPUT);
+  pinMode(player2, OUTPUT);
+  pinMode(statusLED, OUTPUT);
   
-  delay(3000);
+  start();
+}
+
+void start() {
+  justShifted = false;
+  
+  //start at -1 so we being with the first note
+  curNote = -1; 
+  curRep = 0;
+  curPhase = 0;
+  
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(statusLED, HIGH);
+    delay(250);
+    digitalWrite(statusLED, LOW);
+    delay(250);
+  }
   
   lastBeat = millis();
   timePassed = millis();
@@ -36,29 +55,36 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  update();
-  
-  if (p1[curNote]) digitalWrite(led1, HIGH);
-  if (p2[curNote]) digitalWrite(led2, HIGH);
-  
-  delay(beatLength/2);
-  
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
+  if (curPhase <= numPhases) {
+    update();
+    
+    if (pattern1[curNote]) digitalWrite(player1, HIGH);
+    if (pattern2[curNote]) digitalWrite(player2, HIGH);
+    
+    delay(beatLength/2);
+    
+    digitalWrite(player1, LOW);
+    digitalWrite(player2, LOW);
+  }
 }
 
 
 void update() {
   timePassed = millis() - lastBeat;
+  
+  if (justShifted) digitalWrite(statusLED, HIGH);
+  
   while(timePassed < beatLength) {
     timePassed = millis() - lastBeat;
   }
   
+  if (justShifted) digitalWrite(statusLED, LOW); justShifted = false;
+  
   curNote++;
-  if (curNote == numNotes) {
+  if (curNote >= numNotes) {
     curNote = 0;
     curRep++;
-    if (curRep == numReps) {
+    if (curRep >= numReps) {
       curRep = 0;
       curPhase++;
       shift();
@@ -69,9 +95,10 @@ void update() {
 }
 
 void shift() {
-  int temp = p2[0];
+  int temp = pattern2[0];
   for (int i = 0; i < numNotes-1; i++) {
-    p2[i] = p2[i+1];
+    pattern2[i] = pattern2[i+1];
   }
-  p2[numNotes-1] = temp;
+  pattern2[numNotes-1] = temp;
+  justShifted = true;
 }
